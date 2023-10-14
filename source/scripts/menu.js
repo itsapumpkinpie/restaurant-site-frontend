3346edcc85c9cf9095b7f9a42fd2c49915e38db1
itsapumpkinpie <itsapumpkinpie@gmail.com>
2023-10-14 16:04:34 +0300

    js

--- source/scripts/menu.js
+++ source/scripts/menu.js
@@ -1,21 +1,22 @@
 const url = 'http://127.0.0.1:8000/api/v1/menulist';
 
-// async function fetchHandler() {
-//     try {
-//         const response = await fetch(url , {
-//             mode: 'no-cors',
-//           })
-//         console.log(response);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// fetchHandler();
-
-fetch(url).then((response) => {
-    return response.json();
-}).then((data) => {
-    // console.log(data);
-});
-
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Ошибка сети: ' + response.status);
+    }
+    return response.json(); // Парсим ответ как JSON
+  })
+  .then(data => {
+    // Обрабатываем данные с сервера
+    data['results'].forEach(item => {
+      const dish_name = item.dish_name;
+      const photo_path = item.photo_path;
+      const cat = item.cat;
+      console.log(dish_name);
+    });
+  })
+  .catch(error => {
+    console.error('Произошла ошибка:', error);
+  });
+  
