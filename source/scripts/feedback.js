b0020ef12c066ad9a061c58328d03e4f97fa704e
itsapumpkinpie <itsapumpkinpie@gmail.com>
2023-10-19 16:45:55 +0300

    favicon

--- source/scripts/feedback.js
+++ source/scripts/feedback.js
@@ -1,63 +1,28 @@
-let apiURL = 'http://127.0.0.1:8000/api/v1/feedbacklist';
-
-function itemCardGeneration(item) {
-    const cardContainer = document.getElementById("card-container");
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("col-lg-4");
-    cardElement.classList.add("col-md-6");
-    cardElement.classList.add("mt-4");
-    cardElement.innerHTML = `
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${item.user_name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${item.time_create}</h6>
-                <p class="card-text">${item.content}</p>
-            </div>
-        </div>
-        `;
-    cardContainer.appendChild(cardElement);
-}
+const url = 'http://127.0.0.1:8000/api/v1/feedbacklist';
 
 
-{/* < button class="btn btn-primary" type = "button" id = "prev-page" > Предыдущая страница</button >
-    <button class="btn btn-primary" type="button" id="next-page">Следующая страница</button> */}
+document.getElementById('Form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        function loadData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    let dataContainer = document.getElementById('card-container');
-                    dataContainer.innerHTML = '';
-                    data['results'].forEach(item => {
-                        itemCardGeneration(item)
-                    });
-                    let pagination = document.getElementById('pagination');
-                    pagination.innerHTML = '';
-                    if (data.previous) {
-                        let prevButton = document.createElement('button');
-                        prevButton.textContent = 'Назад';
-                        prevButton.classList.add("btn");
-                        prevButton.classList.add("btn-primary");
-                        prevButton.addEventListener('click', function () {
-                            loadData(data.previous);
-                        });
-                        pagination.appendChild(prevButton);
-                    }
+    var formData = new FormData(this);
+    var formDataObj = {};
 
-                    if (data.next) {
-                        let nextButton = document.createElement('button');
-                        nextButton.textContent = 'Вперёд';
-                        nextButton.classList.add("btn");
-                        nextButton.classList.add("btn-primary");
-                        nextButton.addEventListener('click', function () {
-                            loadData(data.next);
-                        });
-                        pagination.appendChild(nextButton);
-                    }
-                })
-                .catch(error => {
-                    console.error('Произошла ошибка:', error);
-                });
+    formData.forEach(function (value, key) {
+        formDataObj[key] = value;
+    });
+
+    var jsonData = JSON.stringify(formDataObj);
+
+    fetch(url, {
+        method: 'POST',
+        body: jsonData,
+        headers: {
+            'Content-Type': 'application/json'
         }
-
-loadData(apiURL);
+    })
+        .then(response => response.json())
+        .then(data => {
+            this.reset()
+            console.log(data);
+        });
+});
